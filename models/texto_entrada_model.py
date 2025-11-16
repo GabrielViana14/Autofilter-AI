c2e28f9eee74b85a55fa54d195fe578938dfd438
gabrielviana14 <gabrielantonio1423@gmail.com>
2025-11-16 18:32:44 -0300

    refactor: rename 'texto' to 'text' in TextoEntradaModel and remove unused models

--- models/texto_entrada_model.py
+++ models/texto_entrada_model.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel
 
 class TextoEntradaModel(BaseModel):
-    texto: str
+    text: str
--- models/texto_saida_model.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from models.carro_model import CarroModel
-
-class TextoSaidaModel(BaseModel):
-    texto: str
-    recomendacoes: list[CarroModel]
